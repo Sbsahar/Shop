705e4fcd9d69be4622659b224eb3afe232318aee
Sbsahar <samo3696385@gmail.com>
2025-03-11 18:15:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import telebot
 import time
-from handlers import *  # استيراد جميع المعالجات
+from handlers import *  # استيراد جميع المعالجات من handlers.py
 
 # إعداد البوت
 bot = telebot.TeleBot("7383597510:AAHsQBH05yVEN96rL783BDkRD1YO1yXY1R4")
@@ -12,11 +12,13 @@ DEVELOPER_ID = "6789179634"
 user_data = {}
 user_ids = set()
 
+print("جاري تسجيل المعالجات...")  # للتأكد من أن الكود يصل إلى هنا
+
 if __name__ == "__main__":
     print("البوت يعمل الآن...")
     while True:
         try:
-            bot.polling(none_stop=True)
+            bot.polling(none_stop=True, interval=0, timeout=20)
         except Exception as e:
             print(f"حدث خطأ: {e}")
             time.sleep(5)
